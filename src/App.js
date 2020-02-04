ce9a9aa4d72458873c581f041226bcb3490e04ab
Elinda Sintaresmi <elinda.sinta@gmail.com>
2020-02-04 17:50:13 +0700

    delete contact map

--- src/App.js
+++ src/App.js
@@ -8,13 +8,11 @@ import Team from './pages/Team';
 import Gallery from './pages/Gallery';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
-import ContactMap from './components/ContactMap'
 
 function App() {
   return (
     <Router>
       <Header />
-      <ContactMap />
       <Switch>
         <Route exact path="/home">
           <Home />
